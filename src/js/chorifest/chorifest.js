03001af6de7f12a24ffd72af61dd7399a4c66929
matias roldan <mroldan@fabricarg.com>
2024-02-02 23:22:02 -0300

    modificacion en scripts, correccion de erroes con urls

--- src/js/chorifest/chorifest.js
+++ src/js/chorifest/chorifest.js
@@ -1,21 +1,84 @@
+//TIPO DE CHORIS
 getTipoChori(showTipoChori);
 
-function showTipoChori(tipoChori) {
+function showTipoChori(listaTipoChoris) {
     const tipoChoriContent = document.getElementById('tipoChori');
-    const newTipoChori = new TipoChori(tipoChori);
-    const template = getTemplate(newTipoChori);
-    tipoChoriContent.append(template);
+    tipoChoriContent.innerHTML = '';
+
+    const mainContainer = document.createElement('div');
+    mainContainer.className = 'rol-selector';
+
+    const spanElement = document.createElement('span');
+    spanElement.textContent = 'TIPO DE CHORI';
+    mainContainer.appendChild(spanElement);
+
+    const selectContainer = document.createElement('div');
+    selectContainer.className = 'form-group select';
+
+    const selectElement = document.createElement('select');
+    selectElement.className = 'option';
+
+    listaTipoChoris.forEach((tipoChori, index) => {
+        const optionElement = document.createElement('option');
+        optionElement.value = index + 1;
+        optionElement.textContent = tipoChori;
+        selectElement.appendChild(optionElement);
+    });
+
+    selectContainer.appendChild(selectElement);
+    mainContainer.appendChild(selectContainer);
+    tipoChoriContent.appendChild(mainContainer);
 }
 
-function getTemplate(tipoChori) {
-    const divAboutMeContentDetail = document.createElement('div');
-    divAboutMeContentDetail.className = 'form-group select';
-    divAboutMeContentDetail.innerHTML = `                                       
-        <div class="form-group select">
-            <select>
-                <option class="option" value="1">${tipoChori.TipoDeChori}</option>
-            </select>
-        </div>
-    `;
-    return divAboutMeContentDetail;
+function getRolSelectorChoris(tipoChori) {
+    const optionElement = document.createElement('option');
+    optionElement.className = 'option';
+    optionElement.value = tipoChori.IdChori;
+    optionElement.innerText = tipoChori.TipoDeChori;
+    return optionElement;
+}
+
+//----------------------------------------------------------------
+
+getRol(showRoles);
+
+
+// Función para mostrar los roles en el contenido
+function showRoles(listaRoles) {
+    const rolContent = document.getElementById('roles');
+    rolContent.innerHTML = '';
+
+    const mainContainer = document.createElement('div');
+    mainContainer.className = 'rol-selector';
+
+    const spanElement = document.createElement('span');
+    spanElement.textContent = 'ROL';
+    mainContainer.appendChild(spanElement);
+
+    const selectContainer = document.createElement('div');
+    selectContainer.className = 'form-group select';
+
+    const selectElement = document.createElement('select');
+    selectElement.className = 'option';
+
+    listaRoles.forEach((rol, index) => {
+        const optionElement = document.createElement('option');
+        optionElement.value = index + 1;
+        optionElement.textContent = rol;
+        selectElement.appendChild(optionElement);
+    });
+
+    selectContainer.appendChild(selectElement);
+    mainContainer.appendChild(selectContainer);
+    rolContent.appendChild(mainContainer);
+}
+
+
+// Función para obtener el selector de roles
+function getRolSelector(rol) {
+    const optionElement = document.createElement('option');
+    optionElement.className = 'option';
+    optionElement.value = rol.IdRol;
+    optionElement.innerText = rol.NombreRol;
+    return optionElement;
 }
